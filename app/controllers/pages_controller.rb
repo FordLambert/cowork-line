b1ae1a0e24c5822f8e9ec126ff548bf203e80755
FordLambert <ford.lambert@protonmail.ch>
2018-04-17 17:21:01 +0200

    Added user connection options and related controller methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,45 @@
 class PagesController < ApplicationController
-  def home
-  end
+
+    def home
+        if session[:user_id]
+            #session[:user_id] = nil
+            redirect_to '/pages/show'
+        end
+    end
+
+    def show
+        if session[:user_id]
+            @current_user = User.find(session[:user_id])
+        end
+    end
+
+    def add_user
+        user = User.new
+        user.first_name = params[:first_name]
+        user.last_name = params[:last_name]
+        user.email = params[:email]
+        user.password = params[:password]
+        user.phone_number = params[:phone_number]
+        user.biography = params[:biography]
+        user.save
+        session[:user_id] = user.id
+        redirect_to '/pages/show'
+    end
+
+    def login
+        @current_user = User.where(email: params[:email], password: params[:password]).first
+        if @current_user
+            session[:user_id] = @current_user.id
+            flash[:info] = "Content de vous voir #{@current_user.first_name} !"
+            redirect_to "/pages/show"
+        else
+            flash[:info] = "Erreur de connection mon brave."
+            redirect_to root_path
+        end
+    end
+
+    def disconnect
+        session[:user_id] = nil
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
     root 'pages#home'
 
-    get 'pages/create' => 'pages#create'
-    get 'pages/:id'  => 'pages#show'
+    get 'pages/create'
+    get 'pages/show'  => 'pages#show'
+    get 'pages/disconnect' => 'pages#disconnect'
 
-    post 'pages/create' => ''
+    post 'pages/create' => 'pages#add_user'
+    post 'pages/login' => 'pages#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

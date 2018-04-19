e989dbeb5f81053a6cc564b543de29a63dad69da
FordLambert <ford.lambert@protonmail.ch>
2018-04-19 15:46:35 +0200

    heroku domain update for email sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'localhost',
+  domain:               'www.cowork-line_.herokuapp.com',
   user_name:            ENV['gmail_username'],
   password:             ENV['gmail_password'],
   authentication:       'plain',

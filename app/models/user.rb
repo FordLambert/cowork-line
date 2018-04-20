9bcdc11a900a05403e937f7891797c049c6b7d5d
FordLambert <ford.lambert@protonmail.ch>
2018-04-20 11:18:55 +0200

    Postgre SQL corrections for fixing local console

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,12 +28,12 @@ class User < ActiveRecord::Base
 
     # Bccrypt password methods
     def password
-        @password ||= Password.new(password)
+        @password ||= Password.new(password_hash)
     end
 
     def password=(new_password)
         @password = Password.create(new_password)
-        self.password = @password
+        self.password_hash = @password
     end
 
     # User methods
